541d89ab83efd72e2efde0a33c1a13a201e4085a
Natnael Samson <Natnaelsamson41@gmail.com>
2025-12-03 11:01:39 +0300

    Fix error message for pint

--- pint.c
+++ pint.c
@@ -10,7 +10,10 @@ void pint(stack_t **stack_head, unsigned int line_number)
 	(void)line_number;
 
 	if (stack_head == NULL || *stack_head == NULL)
-		return;
+	{
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
 
 	printf("%d\n", (*stack_head)->n);
 }
